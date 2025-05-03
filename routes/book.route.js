9aa89d0eb622d0c54d776f56494c20365931b871
KARAN RANA <103360966+karantec@users.noreply.github.com>
2025-05-03 17:44:42 +0530

    Update book.route.js

--- routes/book.route.js
+++ routes/book.route.js
@@ -13,18 +13,11 @@ const { checkCache } = require("../services/redis");
 const router = express.Router();
 
 // Route for inserting bookmaker odds
-router.get(
-  "/insert-bookmaker/:event_id/:market_id",
-  checkCache,
-  insertBookmakerOddsData
-);
+router.get("/insert-bookmaker/:event_id/:market_id",checkCache,insertBookmakerOddsData);
 router.get("/fetch-bookmaker/:event_id/:market_id", fetchBookmakerOdds);
 // router.get("/fetch-event-with-odds", fetchEventWithOdds);
 // router.get("/fancy-odds/:event_id/:market_id", checkCache, insertFancyOddsData);
-router.get(
-  "/fancy-odds/fetch-and-insert/:event_id/:market_id",
-  storeThenInsertFancyOddsData
-);
+router.get("/fancy-odds/fetch-and-insert/:event_id/:market_id",storeThenInsertFancyOddsData);
 // Route to retrieve fancy odds data from Redis
 router.get("/retrieve/:event_id/:market_id", getFancyDataFromRedis);
 
