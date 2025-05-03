ba484122966205d47e0d597ef3b0c56bbe502f53
RONI-GHOSH <141951636+RONI-GHOSH@users.noreply.github.com>
2025-05-03 16:07:08 +0530

    Update bm_routes.js

--- routes/bm_routes.js
+++ routes/bm_routes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const {
-  insertBookmakerToSqlandRedis,
-  getBookmakerOdds,
+  getBookmakerOdds,insertBookmakerToSqlandRedis ,fetchAndCacheBookmakerOdds
 } = require('../controller/bm_data.controller');
 
 const router = express.Router();
 
 router.get('/insert-bookmaker-to-sql-redis/:event_id/:market_id', insertBookmakerToSqlandRedis);
 router.get('/get-bookmaker-odds-redis-cache/:event_id/:market_id', getBookmakerOdds);
-
+router.get('/cache-bookmaker-odds-redis/:event_id/:market_id', fetchAndCacheBookmakerOdds);
 module.exports = router;
