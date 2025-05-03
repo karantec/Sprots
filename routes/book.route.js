24932b829298b73d5fef2dbdab13f5d75d19bc34
RONI-GHOSH <141951636+RONI-GHOSH@users.noreply.github.com>
2025-05-03 17:49:56 +0530

    Update book.route.js

--- routes/book.route.js
+++ routes/book.route.js
@@ -7,6 +7,7 @@ const {
   //   getFancyDataFromRedis,
   storeThenInsertFancyOddsData,
   getFancyDataFromRedis,
+  fetchFancyOddsCached
 } = require("../controller/Book.Controller");
 const { checkCache } = require("../services/redis");
 
@@ -20,5 +21,8 @@ router.get("/fetch-bookmaker/:event_id/:market_id", fetchBookmakerOdds);
 router.get("/fancy-odds/fetch-and-insert/:event_id/:market_id",storeThenInsertFancyOddsData);
 // Route to retrieve fancy odds data from Redis
 router.get("/retrieve/:event_id/:market_id", getFancyDataFromRedis);
+
+router.get("/fancy/:event_id/:market_id", fetchFancyOddsCached);
+
 
 module.exports = router;
