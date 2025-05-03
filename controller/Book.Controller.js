85efa81517e7be88b6f6a7c1512747178c4a2cc1
RONI-GHOSH <141951636+RONI-GHOSH@users.noreply.github.com>
2025-05-03 17:46:14 +0530

    Update Book.Controller.js

--- controller/Book.Controller.js
+++ controller/Book.Controller.js
@@ -571,6 +571,7 @@ const getFancyDataFromRedis = async (req, res) => {
 module.exports = {
   insertBookmakerOddsData,
   fetchBookmakerOdds,
+  storeThenInsertFancyOddsData,
   storeFancyDataToRedis,
   getFancyDataFromRedis,
   fetchFancyOddsCached,
